e1de3f3ba6908c491a44a82e2d246cef8e272a2b
moriyamasaaaki <moriya-7071@outlook.com>
2020-11-13 02:49:42 +0900

    style(Home): スタイル追加 #4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
 <div class="home">
     <h1>ブックマーク</h1>
-    <div v-for="category in categores" :key="category.id">
-        <ul>
-            <router-link tag="li" :to="{ name: 'CategoryDetail', params: { id: category.id } }">{{ category.title }}</router-link>
-        </ul>
+    <div class="home__category">
+        <div class="home__category-list" v-for="category in categores" :key="category.id" :style="{ border: `thick double ${category.color}` }">
+            <ul>
+                <router-link tag="li" class="home__category-item" :to="{ name: 'CategoryDetail', params: { id: category.id } }">{{ category.title }}</router-link>
+            </ul>
+        </div>
     </div>
     <Modal />
 </div>
@@ -26,18 +28,49 @@ export default Vue.extend({
 
     data(): CategoryType {
         return {
-            categores: []
+            categores: [],
         }
     },
 
     async created() {
         await this.getCategores();
+
     },
 
     methods: {
         getCategores(): void {
             this.categores = this.$store.state.categores;
-        }
+        },
     }
 })
 </script>
+
+<style lang="scss" scoped>
+.home {
+    &__category {
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+        flex-wrap: wrap;
+        width: 100%;
+    }
+
+    ul {
+        padding: 0;
+    }
+
+    &__category-list {
+        width: 48%;
+        border-radius: 8px;
+        margin-bottom: 8px;
+    }
+
+    &__category-item {
+        list-style: none;
+        text-align: center;
+        font-weight: 600;
+        color: #5E5E5E;
+        padding: 40px 8px;
+    }
+}
+</style>
