a8265814196f0f149a6fb3ae023d0c2bd0a932de
moriyamasaaaki <moriya-7071@outlook.com>
2020-12-28 19:40:59 +0900

    feat(Login): ログイン画面追加

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,21 @@
 <template>
 <section class="login">
-    <v-btn @click="signInGoogle()" color="primary">
-        Googleログイン
-    </v-btn>
-    <v-btn @click="signInFacebook()" color="indigo">
-        Facebookログイン
-    </v-btn>
+    <h1 class="hdg">Login/ログイン</h1>
+    <div class="login__buttons">
+        <div class="login__button-left">
+            <v-btn class="login__button" @click="signInGoogle()">
+                <img src="@/assets/google_icon.png" width="20" alt="">
+                Google
+            </v-btn>
+        </div>
+
+        <div class="login__button-right">
+            <v-btn class="login__button" @click="signInFacebook()">
+                <img src="@/assets/facebook.png" width="20" alt="">
+                Facebook
+            </v-btn>
+        </div>
+    </div>
 </section>
 </template>
 
@@ -31,3 +41,35 @@ export default Vue.extend({
     }
 });
 </script>
+
+<style lang="scss" scoped>
+.hdg {
+    font-size: 24px;
+    font-weight: 600;
+    font-family: "Hiragino Kaku Gothic ProN", sans-serif;
+    color: #5E5E5E;
+    padding-left: 8px;
+    text-align: center;
+}
+.login {
+    position: relative;
+    height: 100%;
+    &__buttons {
+        display: flex;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translateY(-50%) translateX(-50%);
+        -webkit-transform: translateY(-50%) translateX(-50%);
+    }
+
+    &__button {
+        margin: 0 80px;
+    }
+
+    img {
+        display: block;
+        margin-right: 8px;
+    }
+}
+</style>
